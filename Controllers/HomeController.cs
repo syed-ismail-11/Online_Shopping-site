4ecca3d95d212bcceb3ec58aa6366eb51471d3e3
thisismax160 <122609142+syed-ismail-11@users.noreply.github.com>
2023-11-13 12:39:29 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online_Shopping_site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
